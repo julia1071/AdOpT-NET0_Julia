217eff8e456651f0e096e24b7245e28803455646
julia1071 <80682741+julia1071@users.noreply.github.com>
2025-08-21 15:16:31 +0200

    emission scope for RDS3

--- run_emissionscope.py
+++ run_emissionscope.py
@@ -8,7 +8,7 @@ from adopt_net0.utilities import fix_installed_capacities, installed_capacities_
     installed_capacities_existing_from_file
 
 #Run Chemelot cluster case
-execute = 0
+execute = 1
 
 if execute == 1:
     # Specify the path to your input data
@@ -19,11 +19,11 @@ if execute == 1:
     node = 'Chemelot'
     scope3 = 0
     run_with_emission_limit = 1
-    intervals = ['2040', '2050']
-    # intervals = ['2030', '2040', '2050']
+    # intervals = ['2040', '2050']
+    intervals = ['2030', '2040', '2050']
     interval_emissionLim = {'2030': 1, '2040': 0.5, '2050': 0}
     nr_DD_days = 10
-    take_prev_solution = 1
+    take_prev_solution = 0
     emission_2030 = 2125515.334
     # emission_2040 = 1587293.557
     pyhub = {}
@@ -60,7 +60,7 @@ if execute == 1:
         # solver settings
         model_config['solveroptions']['timelim']['value'] = 24*30
         model_config['solveroptions']['mipgap']['value'] = 0.01
-        model_config['solveroptions']['threads']['value'] = 11
+        model_config['solveroptions']['threads']['value'] = 16
         model_config['solveroptions']['nodefilestart']['value'] = 200
 
         #change save options
@@ -112,7 +112,7 @@ if execute == 1:
 
 
 #Run Chemelot cluster case brownfield
-execute = 0
+execute = 1
 
 
 if execute == 1:
@@ -164,7 +164,7 @@ if execute == 1:
         # solver settings
         model_config['solveroptions']['timelim']['value'] = 24*30
         model_config['solveroptions']['mipgap']['value'] = 0.01
-        model_config['solveroptions']['threads']['value'] = 11
+        model_config['solveroptions']['threads']['value'] = 16
         model_config['solveroptions']['nodefilestart']['value'] = 200
 
         #change save options
